2a866841a40a9ddcea4a45697e67ef9dee86a39c
cynzhra <cynthiazhra@gmail.com>
2023-12-09 09:33:16 +0700

    menghapus file

--- message.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("asset/style.css");
-
-if(isset($_SESSION['message'])){
-    echo "<div class='alert alert-danger' role='alert'>".$_SESSION['message']."</div>";
-    unset($_SESSION["message"]);
-}
